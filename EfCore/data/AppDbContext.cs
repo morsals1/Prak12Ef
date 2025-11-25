0c8cbe2e78afe5bddaba842662a032d178702381
morsals1 <menshik202@gmail.com>
2025-11-25 10:04:24 +0500

    needWork

---
+++ EfCore/data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=sql.ects;Database=DbMenshikov;User Id=student_12;Password=student_12;TrustServerCertificate=True;");
+        }
+    }
+}
